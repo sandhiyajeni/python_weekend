8afde28aa15565484e1b89a3893a3737af8b3818
sandhiyajeni <sandhiya.p@itvedant.com>
2024-01-28 15:07:45 +0530

    first

---
+++ list.py
@@ -0,0 +1,29 @@
+a=[1,2,3,4,5]
+for i in a:
+    print(i)
+print()
+
+print("indexing",a[-1])
+print("repitition",a*2)
+b=[6,7]
+c=a+b
+print("concat",c)
+#adding
+a.append(8)
+print(a)
+a.extend(b)
+print(a)
+a.insert(5,7)
+print(a)
+a.insert(2,b)
+print(a)
+print(a[2][0])
+#removing
+a.remove(7)
+print(a)
+a.pop(0)
+print(a)
+a.clear()
+print(a)
+del a
+print(a)
